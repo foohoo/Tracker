b8dbf6adba342f111aa6a9ea38a722f41d7cb5b8
Dexter <foohoo@gmail.com>
2015-01-25 21:47:26 +0000

    DB Added a couple more levels and refactored the level drawerer

--- Main.py
+++ Main.py
@@ -1,11 +1,41 @@
 __author__ = 'Baron'
 
-import pygame, sys, Tracker
+import pygame, sys, Tracker, glob
 from pygame.locals import *
 from Tracker import *
 
 pygame.init()
 pygame.display.set_caption('Tracker')
+
+
+def display_message(text):
+    textSurfaceObj = fontObj.render(text, True, TRONBLUELIGHT, BLACK)
+    textRectObj = textSurfaceObj.get_rect()
+    textRectObj.center = (WIN_WIDTH / 2, WIN_HEIGHT / 2)
+    DISPLAYSURF.blit(textSurfaceObj, textRectObj)
+
+
+def draw_level(levelNo):
+    level = Level(levels[levelNo])
+    for wall in level.walls:
+        pygame.draw.rect(DISPLAYSURF, TRONBLUEDARK, wall)
+
+    return level
+
+
+def get_levels():
+    levels = []
+    map_files = glob.iglob("./*.txt")
+
+    for map_file in map_files:
+        with open(map_file) as level_txt:
+            map_data = []
+            for line in level_txt:
+                map_data.append(line.strip())
+
+        levels.append(map_data)
+
+    return levels
 
 #Set Constants
 WIN_WIDTH = 912
@@ -13,62 +43,34 @@ WIN_HEIGHT = 608
 
 DARKBLUE = (0, 25, 82)
 RED = (255, 0, 0)
+DARKRED = (97, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 128)
+BLACK = (5, 13, 16)
+TRONBLUEDARK = (52, 96, 141)
+TRONBLUELIGHT = (24, 202, 230)
 #End Constants
 
 #Setup game surface
 DISPLAYSURF = pygame.display.set_mode((900, 600))
-DISPLAYSURF.fill(DARKBLUE)
+DISPLAYSURF.fill(BLACK)
 FPS = 150
 fpsClock = pygame.time.Clock()
 #End setup game surface
 
 #global variables
-
 fontObj = pygame.font.Font('freesansbold.ttf', 95)
-textSurfaceObj = fontObj.render('Game Over!', True, RED, DARKBLUE)
-textRectObj = textSurfaceObj.get_rect()
-textRectObj.center = (WIN_WIDTH / 2, WIN_HEIGHT / 2 )
-
-walls = []
-level = []
-
-with open('level1.txt') as leveltxt:
-    for line in leveltxt:
-        level.append(line.strip())
-
+levelNo = 0
+levels = get_levels()
 #end global variables
 
+level = draw_level(levelNo)
 
-class Wall(object):
-
-    def __init__(self, pos):
-        walls.append(self)
-        self.rect = pygame.Rect(pos[0], pos[1], 16, 16)
-
-x = y = 48
-for row in level:
-    for col in row:
-        if col == "W":
-            Wall((x, y))
-        if col == "E":
-            end_rect1 = pygame.Rect(x, y, 16, 16)
-            end_rect2 = pygame.Rect(x, y, 16, 16)
-        if col == "S":
-            starty = y
-        x += 16
-    y += 16
-    x = 48
-
-for wall in walls:
-    pygame.draw.rect(DISPLAYSURF, (255, 255, 255), wall.rect)
-
-tracker = Tracker(WIN_HEIGHT, WIN_WIDTH, starty-4)
-player = pygame.draw.rect(DISPLAYSURF, RED, tracker.player)
+tracker = Tracker(WIN_HEIGHT, WIN_WIDTH, level.starty-8)
+pygame.draw.rect(DISPLAYSURF, TRONBLUELIGHT, tracker.player)
 
 while True:
-    while not tracker.gameEnded:
+    while not tracker.dead and not tracker.win:
 
         tracker.move_player()
 
@@ -76,25 +78,38 @@ while True:
             if event.type == KEYDOWN:
                 tracker.check_direction(event.key)
 
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
 
-        player = pygame.draw.rect(DISPLAYSURF, RED, tracker.player)
-
-        tracker.check_collision(walls, end_rect1, end_rect2)
+        pygame.draw.rect(DISPLAYSURF, TRONBLUELIGHT, tracker.player)
+        tracker.check_collision(level.walls, level.end_rect1, level.end_rect2)
 
         pygame.display.update()
         fpsClock.tick(FPS)
 
     #game has ended
+    if tracker.dead:
+        display_message('Game Over!')
+    else:
+        display_message('Complete!')
+        levelNo += 1
 
-    DISPLAYSURF.blit(textSurfaceObj, textRectObj)
-    for event in pygame.event.get():
-            if event.type == KEYDOWN:
-                if event.key == K_RETURN:
-                    DISPLAYSURF.fill(DARKBLUE)
-                    tracker = Tracker(WIN_HEIGHT, WIN_WIDTH)
+    while tracker.dead or tracker.win:
+        for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
 
-    pygame.display.update()
-    fpsClock.tick(FPS)
+                    if event.key == K_RETURN:
+                        DISPLAYSURF.fill(BLACK)
+                        level = draw_level(levelNo)
+                        tracker = Tracker(WIN_HEIGHT, WIN_WIDTH, level.starty-8)
+
+        pygame.display.update()
+        fpsClock.tick(FPS)
--- Tracker.py
+++ Tracker.py
@@ -4,6 +4,32 @@ import pygame, sys
 from pygame.locals import *
 
 
+class Level():
+
+    def __init__(self, level):
+        self.walls = []
+        self.starty = 0
+        self.end_rect1 = pygame.Rect(0, 0, 16, 16)
+        self.end_rect2 = pygame.Rect(0, 0, 16, 16)
+
+        self.load_level(level)
+
+    def load_level(self, level):
+        x = y = 48
+        for row in level:
+            for col in row:
+                if col == "W":
+                    self.walls.append(pygame.Rect(x, y, 16, 16))
+                if col == "E":
+                    self.end_rect1 = pygame.Rect(x, y, 16, 16)
+                    self.end_rect2 = pygame.Rect(x, y, 16, 16)
+                if col == "S":
+                    self.starty = y
+                x += 16
+            y += 16
+            x = 48
+
+
 class Player():
 
     def __init__(self, x, y):
@@ -17,7 +43,8 @@ class Tracker():
         self.player = Player(10, starty)
         self.win_height = win_height
         self.win_width = win_width
-        self.gameEnded = False
+        self.dead = False
+        self.win = False
 
     def move_player(self):
         if self.direction == 'right':
@@ -47,16 +74,16 @@ class Tracker():
 
     def check_collision(self, walls, endrect1, endrect2):
         if self.player.rect.y < 0 or self.player.rect.y > self.win_height:
-            self.gameEnded = True
+            self.dead = True
 
         if self.player.rect.x < 0 or self.player.rect.x > self.win_width:
-            self.gameEnded = True
+            self.dead = True
 
         for wall in walls:
-            if self.player.rect.colliderect(wall.rect):
-                self.gameEnded = True
+            if self.player.rect.colliderect(wall):
+                self.dead = True
 
         if self.player.rect.colliderect(endrect1) or self.player.rect.colliderect(endrect2):
-            self.gameEnded = True
+            self.win = True
 
         return
